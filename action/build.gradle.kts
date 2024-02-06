ad7b041045d2c0d72f003e94ff7e23638fa7e2db
uwuvern <uwuvern@outlook.com>
2024-02-06 09:32:17 +0000

    Maybe works?

--- action/build.gradle.kts
+++ action/build.gradle.kts
@@ -1,23 +1,10 @@
-plugins {
-    id("java")
-    `maven-publish`
-}
-
-group = properties["group"] as String
-version = properties["version"] as String
-
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
-}
+/*
+ * Copyright (c) 2024 Ashley (uwuvern) <uwuvern@outlook.com>
+ *
+ * This project is licensed under the MIT license, check the root of the project for
+ * more information.
+ */
 
 dependencies {
     implementation(project(":common"))
-
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2024 Ashley (uwuvern) <uwuvern@outlook.com>
+ *
+ * This project is licensed under the MIT license, check the root of the project for
+ * more information.
+ */
+
+plugins {
+    id("java")
+    `maven-publish`
+}
+
+allprojects {
+    group = properties["group"] as String
+    version = properties["version"] as String
+}
+
+subprojects {
+    apply {
+        plugin("java")
+        plugin("maven-publish")
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    val testImplementation by configurations
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.10.2"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testImplementation("org.junit.jupiter:junit-jupiter-api")
+        testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    }
+
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = properties["java"] as String
+        targetCompatibility = properties["java"] as String
+
+        options.encoding = "UTF-8"
+    }
+
+    tasks.withType<Jar> {
+        manifest {
+            attributes["Implementation-Title"] = project.name
+            attributes["Implementation-Version"] = project.version
+        }
+    }
+
+
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group as String
+                artifactId = project.name
+                version = project.version as String
+
+                from(components["java"])
+            }
+        }
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+}
